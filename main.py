5ce5745ede14f892c3b9d9d256d6b325d57711e0
hjyfly <88010326+hjyfly@users.noreply.github.com>
2021-07-27 09:20:14 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,159 @@
+import os
+import argparse
+import json
+import time
+import numpy as np
+
+import torch
+
+from datasets import load_dataset, load_metric
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification, 
+    EvalPrediction, 
+    TrainingArguments
+)
+
+from methods.mixup.utils import MixupAutoModelForSequenceClassification, MixupTrainer
+from utils import *
+        
+if __name__ == '__main__':
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(0)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--seed', default=0, type=int, help='Seed')
+    parser.add_argument('--model', default='bert-base', type=str, help='only bert-base available')
+    parser.add_argument('--data_ratio', default='1.0', type=str, help='data ratio')
+    parser.add_argument('--data_dir', default='data', type=str, help='data directory')
+    parser.add_argument('--dataset', default='ynat', type=str, help='ynat, hateful')
+    parser.add_argument('--output_dir', default='checkpoint/', type=str, help='Checkpoint directory/')
+    parser.add_argument('--result_dir', default='results/', type=str, help='Result directory/')
+    parser.add_argument('--lr', default=5e-5, type=float, help='Learning rate')
+    parser.add_argument('--wr', default=0.0, type=float, help='Warm-up ratio')
+    parser.add_argument('--wd', default=0.01, type=float, help='Weight decay coefficient')
+    parser.add_argument('--batch_size', default=8, type=int, help='Batch size [8, 16, 32]')
+    parser.add_argument('--total_epochs', default=10, type=int, help='Number of epochs')
+    parser.add_argument('--mixup', default=False, type=str2bool, help="Mixup Method")
+    parser.add_argument('--label_smoothing', default=0.0, type=float, help="Label smoothing factor")
+
+    p_args = parser.parse_args()
+
+    start = time.time()
+
+    set_seed(p_args.seed)
+    if not os.path.exists(p_args.result_dir):
+        os.makedirs(p_args.result_dir)
+
+    task_to_keys = {
+        "ynat": ("title", None),
+        "hateful": ("title", "comments")
+    }
+
+    sentence1_key, sentence2_key = task_to_keys[p_args.dataset]
+    max_sequence_length = 128
+    label_column_name = "label"
+        
+    # Load the dataset
+    data_dir = f"{p_args.data_dir}/{p_args.dataset}"
+    data_files = {"train": [], "validation": []}
+    data_files["train"].append(f"{data_dir}/{p_args.data_ratio}/train.json")
+    data_files["validation"].append(f"{data_dir}/validation.json")
+    datasets = load_dataset(path="json", data_dir=data_dir, data_files=data_files, field='data')
+
+    # Load the metric
+    metric = load_metric('./metric.py')
+
+    def compute_metrics(p: EvalPrediction):
+        preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+        preds = np.argmax(preds, axis=1)
+        result = metric.compute(predictions=preds, references=p.label_ids)
+        if len(result) > 1:
+            result["combined_score"] = np.mean(list(result.values())).item()
+        return result
+
+    # Load the pre-trained model
+    label_list = []
+    label_list = get_label_list(datasets["train"][label_column_name])
+    num_labels = len(label_list)
+
+    if p_args.mixup:
+        model = MixupAutoModelForSequenceClassification.from_pretrained(f"klue/{p_args.model}", num_labels=num_labels)
+    else:
+        model = AutoModelForSequenceClassification.from_pretrained(f"klue/{p_args.model}", num_labels=num_labels)
+    model.to(device)
+
+    tokenizer = AutoTokenizer.from_pretrained(f"klue/{p_args.model}")
+
+    def preprocess_function(examples):
+        target = ((examples[sentence1_key],) if sentence2_key is None else (examples[sentence1_key], examples[sentence2_key]))
+        
+        result = tokenizer(*target, padding=True, max_length=max_sequence_length, truncation=True)
+
+        return result
+
+    preprocessed_datasets = datasets.map(preprocess_function, batched=True)
+
+    train_dataset = preprocessed_datasets['train']
+    valid_dataset = preprocessed_datasets['validation']
+
+    args = TrainingArguments(
+        output_dir=p_args.output_dir, evaluation_strategy='epoch', learning_rate=p_args.lr,
+        per_device_train_batch_size=p_args.batch_size, per_device_eval_batch_size=p_args.batch_size,
+        num_train_epochs=p_args.total_epochs, weight_decay=p_args.wd,
+        warmup_ratio=p_args.wr, seed=p_args.seed, save_total_limit=1,
+        logging_strategy="no", label_smoothing_factor=p_args.label_smoothing
+    )
+
+    if p_args.mixup:
+        trainer = MixupTrainer(
+            model, args, train_dataset=train_dataset, eval_dataset=valid_dataset,
+            tokenizer=tokenizer, compute_metrics=compute_metrics,
+        )
+    else:
+        trainer = CustomTrainer(
+            model, args, train_dataset=train_dataset, eval_dataset=valid_dataset,
+            tokenizer=tokenizer, compute_metrics=compute_metrics,
+        )
+
+    trainer.train()
+    trainer.evaluate()
+
+    log_history = trainer.state.log_history
+
+    elapsed_time = (time.time() - start) / 60 # Min.
+
+    log_dir = os.path.join(p_args.result_dir, p_args.dataset)
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    log_dirs = os.listdir(log_dir)
+
+    if len(log_dirs) == 0:
+        idx = 0
+    else:
+        idx_list = sorted([int(d.split('_')[0]) for d in log_dirs])
+        idx = idx_list[-1] + 1
+
+    from time import strftime
+
+    cur_log_dir = '%d_%s' % (idx, strftime('%Y%m%d-%H%M'))
+    full_log_dir = os.path.join(log_dir, cur_log_dir)
+
+    if not os.path.exists(full_log_dir):
+        os.mkdir(full_log_dir)
+
+    path = f'{full_log_dir}/model_{p_args.model}_lr_{p_args.lr}_wr_{p_args.wr}_wd_{p_args.wd}_bs_{p_args.batch_size}_te_{p_args.total_epochs}_ls_{p_args.label_smoothing}_mixup_{str(p_args.mixup)}.json'
+
+    with open(path, 'w') as f:
+        result = {
+            'seed': p_args.seed,
+            'time': elapsed_time,
+            'results': log_history,
+        }
+
+        json.dump(result, f, indent=2)
---
+++ metric.py
@@ -0,0 +1,89 @@
+import re
+import string
+import collections
+
+from scipy.stats import pearsonr
+from sklearn.metrics import f1_score, average_precision_score, accuracy_score
+from seqeval.metrics import classification_report
+
+import datasets
+
+class KLUE(datasets.Metric):
+    def _info(self):
+        return datasets.MetricInfo(
+            description="KLUE metric",
+            citation="citation",
+            inputs_description="predictions, references",
+            features=datasets.Features({
+                # https://github.com/huggingface/datasets/blob/master/metrics/glue/glue.py
+                "predictions": datasets.Value("int64"),
+                "references": datasets.Value("int64"),
+            }),
+        )
+
+    def _compute(self, predictions, references):
+        return {
+            "f1": f1_score(y_true=references, y_pred=predictions, average="macro")
+        }
+
+def get_raw_scores(dataset, preds):
+    exact_scores = {}
+    f1_scores = {}
+    for article in dataset:
+        for p in article["paragraphs"]:
+            for qa in p["qas"]:
+                qid = qa["guid"]
+                gold_answers = [t for t in qa["answers"]["text"] if normalize_answer(t)]
+                if not gold_answers:
+                    # For unanswerable questions, only correct answer is empty string
+                    gold_answers = [""]
+                if qid not in preds:
+                    print("Missing prediction for %s" % qid)
+                    continue
+                a_pred = preds[qid]
+                # Take max over all gold answers
+                exact_scores[qid] = max(compute_exact(a, a_pred) for a in gold_answers)
+                f1_scores[qid] = max(compute_f1(a, a_pred) for a in gold_answers)
+    return exact_scores, f1_scores
+
+def normalize_answer(s):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+
+    def remove_articles(text):
+        regex = re.compile(r"\b(a|an|the)\b", re.UNICODE)
+        return re.sub(regex, " ", text)
+
+    def white_space_fix(text):
+        return " ".join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return "".join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+def compute_exact(a_gold, a_pred):
+    return int(normalize_answer(a_gold) == normalize_answer(a_pred))
+
+def compute_f1(a_gold, a_pred):
+    gold_toks = get_tokens(a_gold)
+    pred_toks = get_tokens(a_pred)
+    common = collections.Counter(gold_toks) & collections.Counter(pred_toks)
+    num_same = sum(common.values())
+    if len(gold_toks) == 0 or len(pred_toks) == 0:
+        # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
+        return int(gold_toks == pred_toks)
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(pred_toks)
+    recall = 1.0 * num_same / len(gold_toks)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
+def get_tokens(s):
+    if not s:
+        return []
+    return normalize_answer(s).split()
